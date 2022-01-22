e6efa018eddbd7cc4befb9b5ed10d0ebf88524ea
Chao <37573798+Chao1988@users.noreply.github.com>
2022-01-22 14:54:08 +0800

    Add files via upload
    upload reinstall Big Sur shell script

---
+++ Big Sur.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+diskpoint=$(diskutil list physical |head -n1 | awk -F "/"  '{print $3}' |awk '{print $1}')
+diskutil eject $diskpoint
+diskutil erasedisk APFS "Macintosh HD" $diskpoint
+/Volumes/Reinstall\ macOS/11.6.2/Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD/ --agreetolicense
