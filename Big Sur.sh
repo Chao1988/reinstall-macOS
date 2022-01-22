50fef1d6b3a7f6574edef9a3d517906473d24c18
Chao <37573798+Chao1988@users.noreply.github.com>
2022-01-22 15:00:26 +0800

    Update Big Sur.sh

--- Big Sur.sh
+++ Big Sur.sh
@@ -2,4 +2,4 @@
 diskpoint=$(diskutil list physical |head -n1 | awk -F "/"  '{print $3}' |awk '{print $1}')
 diskutil eject $diskpoint
 diskutil erasedisk APFS "Macintosh HD" $diskpoint
-/Volumes/Reinstall\ macOS/11.6.2/Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD/ --agreetolicense
+/Volumes/Reinstall\ macOS/Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --volume /Volumes/Macintosh\ HD/ --agreetolicense
